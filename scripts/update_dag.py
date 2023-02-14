1e51d61488b10ce97c3f061f59a025ee04025f59
Pawel Sabaj <psab@ecco.com>
2023-02-14 08:56:49 +0100

    .

--- scripts/update_dag.py
+++ scripts/update_dag.py
@@ -1,17 +1,24 @@
-import zipfile
-import json
+def dag_operate(dag_file, rm_file, proj_ver):
+    import zipfile
+    import json
 
-# read dag.json
-with open("dag.json", "r") as f:
-    dag_content = json.load(f)
+    # read dag.json
+    with open(dag_file, "r") as f:
+        dag_content = json.load(f)
 
-# read ecco_cdp_gold_retailmanagement.json
-with open("ecco_cdp_gold_retailmanagement.json", "r") as f:
-    gold_content = json.load(f)
+    # read ecco_cdp_gold_retailmanagement.json
+    with open(rm_file, "r") as f:
+        gold_content = json.load(f)
 
-# overwrite current graph node variable
-gold_content["graph"] = dag_content
+    # overwrite current graph node variable
+    gold_content["graph"] = dag_content
+    gold_content["project_version"] = proj_ver
 
-# save changes in ecco_cdp_gold_retailmanagement.json file
-with open("ecco_cdp_gold_retailmanagement.json", "w") as f:
-    json.dump(gold_content, f, indent=4)
+
+    # save changes in ecco_cdp_gold_retailmanagement.json file
+    with open(rm_file, "w") as f:
+        json.dump(gold_content, f, indent=4)
+
+
+dag_operate(dag_file="dag.json", rm_file="ecco_cdp_gold_retailmanagement.json", proj_ver="rm1.2.3333")
+
